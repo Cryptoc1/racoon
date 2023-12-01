639c0bd571c6a34232344e821a3fad1311aed9a5
Samuel Steele <steelesam72@gmail.com>
2023-12-01 18:55:27 -0500

    support cancellation in CS RCONClientExtensions

--- src/CoreRCON.Extensions.CounterStrike/RCONClientExtensions.cs
+++ src/CoreRCON.Extensions.CounterStrike/RCONClientExtensions.cs
@@ -4,23 +4,23 @@ namespace CoreRCON.Extensions.CounterStrike;
 
 public static class RCONClientExtensions
 {
-    public static Task<string> DSWorkshopChangeLevel(this RCONClient console, string map) => console.SendCommandAsync($"ds_workshop_changelevel {map}");
+    public static Task<string> DSWorkshopChangeLevel(this RCONClient client, string map, CancellationToken cancellation = default) => client.SendCommandAsync($"ds_workshop_changelevel {map}", cancellation);
 
-    public static async Task<string[]> DSWorkshopListMaps(this RCONClient console)
+    public static async Task<string[]> DSWorkshopListMaps(this RCONClient client, CancellationToken cancellation = default)
     {
-        var value = await console.SendCommandAsync("ds_workshop_listmaps");
+        var value = await client.SendCommandAsync("ds_workshop_listmaps", cancellation);
         return value.Split([Environment.NewLine], StringSplitOptions.RemoveEmptyEntries);
     }
 
-    public static Task<string> BotAddCT(this RCONClient console) => console.SendCommandAsync("bot_add_ct");
+    public static Task<string> BotAddCT(this RCONClient client, CancellationToken cancellation = default) => client.SendCommandAsync("bot_add_ct", cancellation);
 
-    public static Task<string> BotAddT(this RCONClient console) => console.SendCommandAsync("bot_add_t");
+    public static Task<string> BotAddT(this RCONClient client, CancellationToken cancellation = default) => client.SendCommandAsync("bot_add_t", cancellation);
 
-    public static Task<string> Exec(this RCONClient console, string config) => console.SendCommandAsync($"exec {config}");
+    public static Task<string> Exec(this RCONClient client, string config, CancellationToken cancellation = default) => client.SendCommandAsync($"exec {config}", cancellation);
 
-    public static Task HostWorkshopMap(this RCONClient console, ulong workshopId) => console.SendCommandAsync($"host_workshop_map {workshopId}");
+    public static Task HostWorkshopMap(this RCONClient client, ulong workshopId, CancellationToken cancellation = default) => client.SendCommandAsync($"host_workshop_map {workshopId}", cancellation);
 
-    public static Task<string> Say(this RCONClient console, string text) => console.SendCommandAsync($"say {text}");
+    public static Task<string> Say(this RCONClient client, string text, CancellationToken cancellation = default) => client.SendCommandAsync($"say {text}", cancellation);
 
-    public static Task<Status> Status(this RCONClient console) => console.SendCommandAsync<Status>("status");
+    public static Task<Status> Status(this RCONClient client, CancellationToken cancellation = default) => client.SendCommandAsync<Status>("status", cancellation);
 }
